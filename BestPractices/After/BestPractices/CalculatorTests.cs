a2ced6e3aaf69788fde288649110ba1f51f3e29b
Niels Ruys <Niels.Ruys@euri.com>
2019-10-23 10:19:23 +0200

    best practices - extreme values

--- BestPractices/After/BestPractices/CalculatorTests.cs
+++ BestPractices/After/BestPractices/CalculatorTests.cs
@@ -1,4 +1,6 @@
+using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace BestPractices
 {
@@ -6,8 +8,9 @@ namespace BestPractices
     {
         [Theory]
         [InlineData(5, 10, 15)]
-        [InlineData(2, 1, 3)]
-        [InlineData(8, 3, 11)]
+        [InlineData(-2, 1, -1)]
+        [InlineData(8, -3, 5)]
+        [InlineData(-8, -3, -11)]
         public void Add_ShouldReturnTheSum(int value1, int value2, int expected)
         {
             var calculator = new Calculator();
@@ -18,9 +21,24 @@ namespace BestPractices
         }
 
         [Theory]
+        [InlineData(int.MinValue, -1)]
+        [InlineData(-1, int.MinValue)]
+        [InlineData(int.MaxValue, 1)]
+        [InlineData(1, int.MaxValue)]
+        public void Add_WithExtremeValues_ShouldThrowOverFlowException(int value1, int value2)
+        {
+            var calculator = new Calculator();
+
+            Action action = () => calculator.Add(value1, value2);
+
+            Assert.Throws<OverflowException>(action);
+        }
+
+        [Theory]
         [InlineData(5, 10, -5)]
-        [InlineData(2, 1, 1)]
-        [InlineData(8, 3, 5)]
+        [InlineData(-2, 1, -3)]
+        [InlineData(8, -3, 11)]
+        [InlineData(-8, -3, -5)]
         public void Subtract_ShouldReturnTheDifference(int value1, int value2, int expected)
         {
             var calculator = new Calculator();
@@ -29,5 +47,20 @@ namespace BestPractices
 
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData(int.MinValue, 1)]
+        [InlineData(1, int.MinValue)]
+        [InlineData(-2, int.MaxValue)]
+        [InlineData(int.MaxValue, -1)]
+        public void Subtract_WithExtremeValues_ShouldThrowOverFlowException(int value1, int value2)
+        {
+            var calculator = new Calculator();
+
+            Action action = () => calculator.Subtract(value1, value2);
+
+            Assert.Throws<OverflowException>(action);
+        }
+
     }
 }
